256a3fd25a07eb68d7f401bf4c17df3675f7ba6f
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-20 09:56:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Hangman_Lite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "COMPUTER";
+            
+            Console.WriteLine("  +---+ ");
+            Console.WriteLine("  |   | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("=========");
+        }
+
+        public static void Hangman1()
+        {
+            Console.WriteLine("  +---+ ");
+            Console.WriteLine("  |   | ");
+            Console.WriteLine("  0   | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("=========");
+        }
+
+        public static void Hangman2()
+        {
+            Console.WriteLine("  +---+ ");
+            Console.WriteLine("  |   | ");
+            Console.WriteLine("  0   | ");
+            Console.WriteLine(" /|\\ | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("=========");
+        }
+
+        public static void Hangman3()
+        {
+            Console.WriteLine("  +---+ ");
+            Console.WriteLine("  |   | ");
+            Console.WriteLine("  0   | ");
+            Console.WriteLine(" /|\\ | ");
+            Console.WriteLine(" / \\ | ");
+            Console.WriteLine("      | ");
+            Console.WriteLine("=========");
+        }
+    }
+}
